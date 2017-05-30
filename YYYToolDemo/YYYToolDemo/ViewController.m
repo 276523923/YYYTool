d38e79c643fd12ec11222c7b00376fc559de1829
276523923 <yyyue@vip.qq.com>
2017-05-30 17:31:01 +0800

    测试 ##001##

--- YYYToolDemo/YYYToolDemo/ViewController.m
+++ YYYToolDemo/YYYToolDemo/ViewController.m
@@ -16,7 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional  wefwe setup after loading the view, typically from a nib.
+    NSLog(@"111");
+    // Do any additional  wefwe 111 setup after loading the view, typically from a nib.
 }
 
 
