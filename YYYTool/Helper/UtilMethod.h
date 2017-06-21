159452bb90b9ecb7a92d0872347c3ca49a3f5985
276523923 <yyyue@vip.qq.com>
2017-06-21 17:47:22 +0800

    优化

--- YYYTool/Helper/UtilMethod.h
+++ YYYTool/Helper/UtilMethod.h
@@ -9,7 +9,6 @@
 #ifndef UtilMethod_h
 #define UtilMethod_h
 #import <UIKit/UIKit.h>
-#import <pthread.h>
 #import "UIDevice+Addition.h"
 
 typedef NS_ENUM(NSUInteger, iPhoneModel) {
@@ -183,32 +182,17 @@ static inline UIFont *LIGHTFONT(CGFloat size){
 }
 
 /**
- 当前是否在主线程
- */
-static inline bool dispatch_is_main_queue() {
-    return pthread_main_np() != 0;
-}
-
-/**
  Submits a block for asynchronous execution on a main queue and returns immediately.
  */
 static inline void dispatch_async_on_main_queue(void (^block)()) {
-    if (pthread_main_np()) {
-        block();
-    } else {
-        dispatch_async(dispatch_get_main_queue(), block);
-    }
+    dispatch_async(dispatch_get_main_queue(), block);
 }
 
 /**
  Submits a block for execution on a main queue and waits until the block completes.
  */
 static inline void dispatch_sync_on_main_queue(void (^block)()) {
-    if (pthread_main_np()) {
-        block();
-    } else {
-        dispatch_sync(dispatch_get_main_queue(), block);
-    }
+    dispatch_sync(dispatch_get_main_queue(), block);
 }
 
 #endif /* UtilMethod_h */
